56f9af4c684399b9fe3e24fedb48c4956cf80169
Bebo <pierrick.bobet@etu.univ-tours.fr>
2017-04-26 16:00:15 +0200

    Update CSommet

--- CGrpahes/CGrpahes/main.cpp
+++ CGrpahes/CGrpahes/main.cpp
@@ -3,48 +3,65 @@
 #include "CParse.h"
 
 #include <vld.h>
+
+void testMethodesGraphe();
+void testMethodesSommet();
+
+// Ne pas garder ce main pour le rendu c'est sale car par .h pour les protos fonctions...
 void main() {
-	// Creation d'un graphe vide
+	//testMethodesGraphe();
+	//testMethodesSommet();
+}
+
+// Il reste les méthodes comme modifier...
+void testMethodesGraphe() {
+	// Declaration des graphes
 	CGraphe * GRANewGraphe1 = new CGraphe();
-	// Création du graphe vide
 	CGraphe * GRANewGraphe2 = new CGraphe();
 
 	try {
+		GRANewGraphe1 = new CGraphe();
+		GRANewGraphe2 = new CGraphe();
+	}
+	catch (CException e) {
+		cout << e.EXCLectureMessage();
+	}
 
-	// TEST 1 - Constructeur par défaut
-	cout << "--- Test du graphe 1 -> Vide : " << endl;
-		// Configuration
-			// Test du graphe initialise
-				// Test si le graphe existe et si NbSommets et NbArcs du graphe = 0
-	if(GRANewGraphe1 != nullptr && GRANewGraphe1->GRAGetNbSommets() != 0 && GRANewGraphe1->GRAGetNbArcs() != 0)
-		throw CException(ECHECNONTROUVE, "Erreur du test");
+	try {
+		// TEST 1 - Constructeur par défaut
+		cout << "--- Test du graphe 1 -> Vide : " << endl;
+			// Configuration
+				// Test du graphe initialise
+					// Test si le graphe existe et si NbSommets et NbArcs du graphe = 0
+		if(GRANewGraphe1 != nullptr && GRANewGraphe1->GRAGetNbSommets() != 0 || GRANewGraphe1->GRAGetNbArcs() != 0)
+			throw CException(ECHECNONTROUVE, "Erreur du test");
 
-			// Affichage du graphe
-	GRANewGraphe1->GRAAfficherGraphe();
+				// Affichage du graphe
+		GRANewGraphe1->GRAAfficherGraphe();
 
-	// TEST 2 - Constructeur de confort
-	cout << "--- Test du graphe 2 -> Non vide : " << endl;
-		// Affectation des 3 sommets au graphe
-	GRANewGraphe2->GRAAjoutSommet(4, nullptr, nullptr);
-	GRANewGraphe2->GRAAjoutSommet(1, nullptr, nullptr);
-	GRANewGraphe2->GRAAjoutSommet(3, nullptr, nullptr);
-	GRANewGraphe2->GRAAjoutSommet(2, nullptr, nullptr);
-		// Vérification du compteur général du nombre de sommet dans le graphe
-	if(GRANewGraphe2->GRAGetNbSommets() != 4)
-		throw CException(ERREURTEST, "Erreur du test, il n'y a pas 3 sommets");
-			// Ajouter des arcs
-				// 1 vers 1
-	GRANewGraphe2->GRAAjoutArc(1, GRANewGraphe2->GRATrouverSommet(1));
-				// 1 vers 3
-	GRANewGraphe2->GRAAjoutArc(3, GRANewGraphe2->GRATrouverSommet(1));
-				// 1 vers 2
-	GRANewGraphe2->GRAAjoutArc(2, GRANewGraphe2->GRATrouverSommet(1));
-				// 1 vers 4
-	GRANewGraphe2->GRAAjoutArc(4, GRANewGraphe2->GRATrouverSommet(1));
-				// 2 vers 1
-	GRANewGraphe2->GRAAjoutArc(1, GRANewGraphe2->GRATrouverSommet(2));
-			// Affichage du graphe
-	GRANewGraphe2->GRAAfficherGraphe();
+		// TEST 2 - Constructeur de confort
+		cout << "--- Test du graphe 2 -> Non vide : " << endl;
+			// Affectation des 3 sommets au graphe
+		GRANewGraphe2->GRAAjoutSommet(4, nullptr, nullptr);
+		GRANewGraphe2->GRAAjoutSommet(1, nullptr, nullptr);
+		GRANewGraphe2->GRAAjoutSommet(3, nullptr, nullptr);
+		GRANewGraphe2->GRAAjoutSommet(2, nullptr, nullptr);
+			// Vérification du compteur général du nombre de sommet dans le graphe
+		if(GRANewGraphe2->GRAGetNbSommets() != 4)
+			throw CException(ERREURTEST, "Erreur du test, il n'y a pas 3 sommets");
+				// Ajouter des arcs
+					// 1 vers 1
+		GRANewGraphe2->GRAAjoutArc(1, GRANewGraphe2->GRATrouverSommet(1));
+					// 1 vers 3
+		GRANewGraphe2->GRAAjoutArc(3, GRANewGraphe2->GRATrouverSommet(1));
+					// 1 vers 2
+		GRANewGraphe2->GRAAjoutArc(2, GRANewGraphe2->GRATrouverSommet(1));
+					// 1 vers 4
+		GRANewGraphe2->GRAAjoutArc(4, GRANewGraphe2->GRATrouverSommet(1));
+					// 2 vers 1
+		GRANewGraphe2->GRAAjoutArc(1, GRANewGraphe2->GRATrouverSommet(2));
+				// Affichage du graphe
+		GRANewGraphe2->GRAAfficherGraphe();
 	}
 	catch (CException e) {
 		cout << e.EXCLectureMessage() << endl;
@@ -62,4 +79,45 @@ void main() {
 	delete(GRANewGraphe1);
 	// Liberation memoire du graphe
 	delete(GRANewGraphe2);
+}
+
+void testMethodesSommet() {
+	// Declaration des attributs
+	CSommet * SOMNewSommetDefaut = nullptr;
+	CSommet * SOMNewSommetConfort = nullptr;
+	CSommet * SOMNewSommetRecopie = nullptr;
+	
+	try {
+		SOMNewSommetDefaut = new CSommet();
+		SOMNewSommetConfort = new CSommet(2, nullptr, nullptr);	
+		SOMNewSommetRecopie = new CSommet(*SOMNewSommetConfort);
+	}
+	catch (CException e) {
+		cout << e.EXCLectureMessage();
+	}
+
+	try {
+		// TEST 1 - Constructeur par defaut
+		cout << "--- Test du sommet 1 -> Vide : " << endl;
+			// Configuration
+				// Test du graphe initialise
+					// Test si le graphe existe et si NbSommets et NbArcs du graphe = 0
+		if(SOMNewSommetDefaut == nullptr || SOMNewSommetDefaut->SOMGetNum() != 0 || SOMNewSommetDefaut->SOMGetNbrArcArrivant() != 0 || SOMNewSommetDefaut->SOMGetNbrArcPartant() != 0)
+			throw CException(ECHECNONTROUVE, "Erreur du test");
+		cout << "Test ok" << endl;
+	}
+	catch (CException e) {
+		cout << e.EXCLectureMessage() << endl;
+	}
+
+	try {
+		// Libération memoire des sommets
+		delete(SOMNewSommetDefaut);
+		delete(SOMNewSommetConfort);
+		// FAIRE UN TEST DANS LE DESTRUCTEUR ARC SI PAS d4ARC ALORS NE RIEN FAIRE !!!!!
+		delete(SOMNewSommetRecopie);
+	}
+	catch (CException e) {
+		cout << e.EXCLectureMessage();
+	}
 }
